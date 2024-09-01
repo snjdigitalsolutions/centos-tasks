65cf29813f5a4baa548f4b9d5b02ab7b78f09414
Jamey <jparham@snjdigitalsolutions.com>
2024-09-01 17:22:09 -0400

    Added -y to DNF commands

--- centos8to9/performUpgrade.sh
+++ centos8to9/performUpgrade.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
-dnf module disable python36 virt
-dnf install https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-release-9.0-26.el9.noarch.rpm https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-gpg-keys-9.0-26.el9.noarch.rpm https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-repos-9.0-26.el9.noarch.rpm
-dnf --releasever=9 --allowerasing --setopt=deltarpm=false distro-sync -y
+dnf -y module disable python36 virt
+dnf -y install https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-release-9.0-26.el9.noarch.rpm /
+    https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-gpg-keys-9.0-26.el9.noarch.rpm /
+    https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-repos-9.0-26.el9.noarch.rpm
+dnf -y --releasever=9 --allowerasing --setopt=deltarpm=false distro-sync
 rpm --rebuilddb
