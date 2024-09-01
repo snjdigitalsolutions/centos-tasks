f3bd63336cfc7a7cf28817537b0dafa8c9f13eba
Jamey <jparham@snjdigitalsolutions.com>
2024-09-01 17:31:30 -0400

    Fix multiline

--- centos8to9/performUpgrade.sh
+++ centos8to9/performUpgrade.sh
@@ -2,8 +2,8 @@
 
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 dnf -y module disable python36 virt
-dnf -y install https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-release-9.0-26.el9.noarch.rpm /
-    https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-gpg-keys-9.0-26.el9.noarch.rpm /
+dnf -y install https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-release-9.0-26.el9.noarch.rpm \
+    https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-gpg-keys-9.0-26.el9.noarch.rpm \
     https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-repos-9.0-26.el9.noarch.rpm
 dnf -y --releasever=9 --allowerasing --setopt=deltarpm=false distro-sync
 rpm --rebuilddb
